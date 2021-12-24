1362d807329ea4c9518a0caf3eab61f785ebe854
Talha Y <tycode0@gmail.com>
2021-12-24 23:33:48 +0100

    completed header

---
+++ app.js
@@ -0,0 +1,20 @@
+const searchBtn = document.querySelector('.btn--search');
+const searchForm = document.querySelector('.search-form');
+
+searchBtn.onclick = () => {
+  searchForm.classList.toggle('active');
+  nav.classList.remove('active');
+};
+
+const menuBtn = document.querySelector('.btn--menu');
+const nav = document.querySelector('nav');
+
+menuBtn.onclick = () => {
+  nav.classList.toggle('active');
+  searchForm.classList.remove('active');
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove('active');
+  nav.classList.remove('active');
+};
